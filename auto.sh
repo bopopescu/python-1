07a1e092aec2be68cb5d2a9f14be9ec6b09b71d2
Yen <qaz951zaq66@gmail.com>
2019-03-15 17:48:41 +0800

    17-48-41

---
+++ auto.sh
@@ -0,0 +1,21 @@
+#! /bin/bash
+
+cd /e/Python/Workspace
+
+while true
+do 
+	fc="$(ls| wc -l)"
+	if [ $fc -gt 0 ];
+		then
+			FILES=/e/Python/Workspace/*
+			for f in $FILES
+			do
+			/mingw64/bin/git add $f
+			/mingw64/bin/git commit -m "$(date +%H-%M-%S)"
+			/mingw64/bin/git remote add origin https://github.com/YenChou/python.git
+			/mingw64/bin/git push -u origin master
+			/mingw64/bin/git remote remove origin
+			done
+		fi;
+		sleep 3
+done
